2fa48c99debe818a58ffa75b8335c7cb9254a0fd
LITMUS7\muhammed.shihab <shihabvk99@gmail.com>
2021-02-10 11:21:12 +0530

    added transation serial number counter for unique serial number

--- lib/main.dart
+++ lib/main.dart
@@ -33,16 +33,17 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   String title;
   String amount;
+  int serialNumber=0;
 
   final List<Transation> _transations = [
     Transation(id: "t1", title: "Car Rent", amount: 2, date: DateTime.now()),
-    Transation(id: "t6", title: "Milk loan", amount: 300, date: DateTime.now()),
-    Transation(id: "t4", title: "Bike loan", amount: 500, date: DateTime.now()),
-    Transation(id: "t5", title: "Bill", amount: 300, date: DateTime.now().subtract(Duration(days: 1))),
-    Transation(id: "t5", title: "Bill", amount: 300, date: DateTime.now().subtract(Duration(days: 2))),
-    Transation(id: "t4", title: "Bike loan", amount: 500, date: DateTime.now()),
-    Transation(id: "t5", title: "Bill", amount: 700, date: DateTime.now().subtract(Duration(days: 3))),
-    Transation(id: "t5", title: "Bill", amount: 900, date: DateTime.now().subtract(Duration(days: 5))),
+    Transation(id: "t2", title: "Milk loan", amount: 300, date: DateTime.now()),
+    Transation(id: "t3", title: "Bike loan", amount: 500, date: DateTime.now()),
+    Transation(id: "t4", title: "shop Bill", amount: 300, date: DateTime.now().subtract(Duration(days: 1))),
+    Transation(id: "t5", title: "car wash Bill", amount: 300, date: DateTime.now().subtract(Duration(days: 2))),
+    Transation(id: "t6", title: "Bike loan", amount: 500, date: DateTime.now()),
+    Transation(id: "t7", title: "grociries Bill", amount: 700, date: DateTime.now().subtract(Duration(days: 3))),
+    Transation(id: "t8", title: "oil mill Bill", amount: 900, date: DateTime.now().subtract(Duration(days: 5))),
   ];
 
   List<Transation> get _recentTransation {
@@ -57,7 +58,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _addNewTrsansation(String title, double amount,DateTime txDate) {
     final newTransaction = Transation(
-        id: DateTime.now().toString() + "tx",
+        id: DateTime.now().toString() + "${serialNumber++}",
         title: title,
         amount: amount,
         date: txDate);
@@ -89,8 +90,9 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-
-    // print(_recentTransation);
+       for(var tx in _transations){
+         print("${tx.id} + ${tx.title}");
+       }
 
     // print(DateTime.now().subtract(Duration(days: 2)));
     // final dateValue = DateTime.now().subtract(Duration(days: 1));
